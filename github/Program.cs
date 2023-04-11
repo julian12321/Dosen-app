95f88afab5fc316a735da84481815004ad39eb26
Julian <julianlistiawan16@students.amikom.ac.id>
2023-04-11 21:39:45 +0700

    first commit

---
+++ github/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+
+            Console.WriteLine("Default Dosen Pengampu:");
+            dosen.display();
+
+            Console.WriteLine("\nMasukka Nama dan NIK Dosen Baru:");
+            dosen.attDosen(Console.ReadLine());
+            dosen.display();
+
+            Console.WriteLine("\nMasukkan Mata Kuliah Baru:");
+            dosen.addCourse(Console.ReadLine());
+            dosen.display();
+
+            Console.ReadLine();
+        }
+    }
+}
